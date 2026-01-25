e94ff6de89c8f49982e9cb01c70248e1c9c40b47
Ilia Krylatykh <iliakrylatykh@gmail.com>
2026-01-25 20:51:48 +0100

    change main music theme

--- src/structure/blocks/setup/SetupSound.ts
+++ src/structure/blocks/setup/SetupSound.ts
@@ -16,10 +16,28 @@ export class SetupSounds extends Block {
 	private setupSounds(): void {
 		import('@pixi/sound').then(({ sound }) => {
 			GAME.sound = sound
+			// BUTTON SFX
+			sound.add('UI-pick', '/assets/audio/SFX/BUTTON_12.wav')
+			sound.volume('UI-pick', 0.01)
 
 			//MUSIC
 			sound.add('music-1', '/assets/audio/music/main_theme.mp3')
-			sound.volume('music-1', 0.1)
+			sound.volume('music-1', 0.03)
+
+			//REEL SFX
+			sound.add('reel-thud', '/assets/audio/SFX/light-thud-super-low.mp3')
+			sound.volume('reel-thud', 0.6)
+
+			// GAME EVENTS
+			sound.add('coin-count-loop', '/assets/audio/SFX/coin-loop-detuned.wav')
+			sound.volume('coin-count-loop', 0.05)
+
+			sound.add('coin-flung', '/assets/audio/SFX/coin-flung-med.mp3')
+			sound.volume('coin-flung', 0.2)
+
+			sound.play('music-1', {
+				loop: true,
+			})
 
 			sound.play('music-1', {
 				loop: true,
